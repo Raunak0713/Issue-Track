028b6e36993943883366514284cea8b340133ab1
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-03-08 08:46:18 +0530

    first commit

---
+++ app/issues/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@radix-ui/themes';
+
+const IssuesPage = () => {
+  return (
+    <div>
+      <Button>
+        New Issue
+      </Button>
+    </div>
+  )
+}
+
+export default IssuesPage
